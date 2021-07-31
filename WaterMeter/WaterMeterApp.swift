ed78ecf25f4b93e637675da62661e5d9c95abbae
phantom-patrick <anton11patrushev@gmail.com>
2021-07-31 19:21:50 +0300

    Initial Commit

---
+++ WaterMeter/WaterMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterMeterApp.swift
+//  WaterMeter
+//
+//  Created by Anton Patrushev on 31.07.21.
+//
+
+import SwiftUI
+
+@main
+struct WaterMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
